72ceb7dc71c6dd81b5afe8e4e80a332a28ee85e2
NaufalTaufiqiAkbar <116734946+NaufalTaufiqiAkbar@users.noreply.github.com>
2023-06-13 14:59:31 +0700

    Initial commit

---
+++ Codes/kresources/ktemplates/frontend/side_nav.php
@@ -0,0 +1,12 @@
+<div class="col-md-3">
+<div class="lead">
+    <img src="../frontend/logo.jpeg" alt="logo" class="logo">
+    </div>
+    <p class="lead">ReThread</p>
+    <div class="list-group">
+        <?php
+            //  <--Category -->
+            get_categories();
+        ?>
+    </div>
+</div>
