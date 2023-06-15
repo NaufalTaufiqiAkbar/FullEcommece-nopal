c6bfdd70dc586293c987ef63c91234e68afae185
NaufalTaufiqiAkbar <naufaltaufiqi12@gmail.com>
2023-06-15 20:19:16 +0700

    update

--- Codes/kresources/ktemplates/frontend/side_nav.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-3">
-    <p class="lead">ReThread</p>
-    <div class="list-group">
-        <?php
-            //  <--Category -->
-            get_categories();
-        ?>
-    </div>
-</div>
